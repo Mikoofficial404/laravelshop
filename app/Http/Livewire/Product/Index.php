19423a3e047cde8d73d4e22c80e1df497ec0c627
Mikoofficial404 <mikoofficial404@gmail.com>
2024-11-08 10:57:09 +0700

    first commit

---
+++ app/Http/Livewire/Product/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use App\Product;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+
+    public $paginate = 10;
+    public $search;
+
+    protected $updatesQueryString = ["search" => ["except"=> ""]];
+
+    public function mount(){
+        $this->search = request()->query("search", $this->search);
+    }
+    public function render()
+    {
+        return view('livewire.product.index',[
+            'products' => $this->search === null ?
+            Product::latest()->paginate($this->paginate) :
+            Product::latest()->where('title', 'like' , '%' . $this->search . '%')
+            ->paginate($this->paginate)
+        ]);
+    }
+}
